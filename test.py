977640da0b499bb893e44e5cea4937d2520922f4
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-02-01 09:52:38 -0500

    Add type information to test case
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- test.py
+++ test.py
@@ -3,7 +3,12 @@
 import sharg
 
 c = sharg.CommandLine(prog="p", example="p cat my_secret")
-o1 = c.add_option("password-store-dir", "P", "path to the password storage directory")
-o2 = c.add_option("gnupg-home-dir", "G", "path to the GnuPG home directory")
+o1 = c.add_option("password-store-dir", "P",
+                  "path to the password storage directory",
+                  sharg.OptionValue.Directory)
+o2 = c.add_option("gnupg-home-dir", "G",
+                  "path to the GnuPG home directory",
+                   sharg.OptionValue.Directory)
 
-c.format_help()
+# c.format_help()
+c.format_bash()
