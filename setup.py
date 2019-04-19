500ecf7dd93843b455a004b43b3472371a4d76c3
Alexander Scheel <alexander.m.scheel@gmail.com>
2019-04-19 00:28:54 -0400

    Add setup.py
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sharg',
+      version='0.1',
+      description='Argument parsing generator targeting bash and man pages',
+      url='http://github.com/cipherboy/sharg',
+      author='Alexander Scheel',
+      author_email='alexander.m.scheel@gmail.com',
+      license='GPLv3',
+      packages=['sharg'],
+      install_requires=[],
+      zip_safe=True)
