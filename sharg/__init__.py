284587d7eeea39f712a84843ae13e430703fdfe1
Alexander Scheel <alexander.m.scheel@gmail.com>
2020-06-28 12:05:58 -0400

    Add flake8 configuration
    Signed-off-by: Alexander Scheel <alexander.m.scheel@gmail.com>

--- sharg/__init__.py
+++ sharg/__init__.py
@@ -1,6 +1,6 @@
 """
 A library to generate command line (shell) argument parsers for a
-variety of target languages.
+variety of target languages and documentation formats.
 
 This currently includes Bash, Bash Completion, and manual pages, but
 could be extended to include any number of languages or libraries.
@@ -15,3 +15,13 @@ from .option import Option
 from .commandline import CommandLine
 
 from .config import parse_yaml, parse_dict
+
+__all__ = [
+    'ShellCodeGen',
+    'ShellConditional',
+    'Value',
+    'Argument',
+    'Option',
+    'CommandLine',
+    'parse_yaml', 'parse_dict'
+]
